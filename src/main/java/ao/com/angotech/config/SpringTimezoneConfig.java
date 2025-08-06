ffd5ec7acad04c7ff9b45522f29dd70b16bf3f72
Fernando Angolar <fernandoangolar581@gmail.com>
2025-08-06 18:33:11 +0100

    :wrench: chore: Configuração do projecto && adicionado a configuraãç do fuso horário e localidade

---
+++ src/main/java/ao/com/angotech/config/SpringTimezoneConfig.java
@@ -0,0 +1,15 @@
+package ao.com.angotech.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class SpringTimezoneConfig {
+
+    @PostConstruct
+    public void timezoneConfig() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Africa/Luanda"));
+    }
+}
